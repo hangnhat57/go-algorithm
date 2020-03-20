a3645fe047f75c1fa2e4ef529ff3845dbda0a11b
Nhat Nguyen Hang <ftt.nhat.nguyen@grabtaxi.com>
2020-03-20 17:46:54 +0700

    Restructure fodler

--- arrays/minimum-swaps-2/main.go
+++ arrays/minimum-swaps-2/main.go
@@ -7,7 +7,9 @@ func main() {
 }
 
 /*
-Given an array of n distinct elements, find the minimum number of swaps required to sort the array.
+You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] without any duplicates.
+You are allowed to swap any two elements.
+You need to find the minimum number of swaps required to sort the array in ascending order.
 */
 
 /*
--- arrays/minimum-swaps-2/main_test.go
+++ arrays/minimum-swaps-2/main_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestMinimumSwap(t *testing.T) {
-	expected := 2
-	actual := minimumSwap([]int{1, 5, 4, 3, 2})
+	expected := 3
+	actual := minimumSwap([]int{4, 3, 1, 2})
 	if expected != actual {
 		t.Error("Expected was", expected, "while actual is", actual)
 	}
